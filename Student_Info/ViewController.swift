cf3377f968c1af32620dcaf822db1c52b06acceb
vvaditya23 <vvaditya23@icloud.com>
2023-05-31 18:07:09 +0530

    Initial Commit

---
+++ Student_Info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student_Info
+//
+//  Created by ヴィヤヴャハレ・アディティア on 31/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
